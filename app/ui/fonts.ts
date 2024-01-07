b469c613b60a64741a8e2bca60220fc7d15f2286
Ariel Antequiera <arielantequiera@gmail.com>
2024-01-07 17:33:55 -0300

    Se modifica vista inicial

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google';
-import { Lusitana } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin']});
-export const lusitana = Lusitana({
+export const poppins = Poppins({
 subsets: ['latin'],
 weight: ['400', '700']})
